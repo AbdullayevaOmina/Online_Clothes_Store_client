625c784f0aa1f24c3e9a97b25db9ac356a499f54
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-05-28 08:17:11 +0500

    ...

--- src/layout/main-layout/index.tsx
+++ src/layout/main-layout/index.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Navbar, UiCategory } from "@components";
 import { MyCarousel, ProductCard } from "@ui";
+
 const index = () => {
   return (
     <>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,18 +6,15 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import { Cart, NotFound, Posts, Products, SignIn, SignUp } from "@pages";
-import { MainLayout } from "@layout";
 
 const Index = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<App />}>
-        <Route path="user/*" element={<MainLayout />}>
-          <Route path="signin" element={<SignIn />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="products" element={<Products />} />
-          <Route path="posts" element={<Posts />} />
-        </Route>
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="products" element={<Products />} />
+        <Route path="posts" element={<Posts />} />
         <Route path="cart" element={<Cart />} />
         <Route path="/*" element={<NotFound />} />
       </Route>
